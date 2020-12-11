b37ca813982a098178e07cc26f60ab7b3287e9af
Илья <bililia555@gmail.com>
2020-12-11 17:12:57 +0300

    commit all files

---
+++ img_to_ASCII/main.cpp
@@ -0,0 +1,78 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace sf;
+using namespace std;
+
+char symbol(int r, int g, int b) {
+	char sym = ' ';
+	int j = 0;
+	char a[17]{ '@', '&', '$', '#', '%', 'B', 'M', '?', '|', '/', '+', '=', '!', ';', ',', '.', ' ' };
+	int k = 12;
+
+	for (int i = 0; i < 17; i++) {
+		if ((r >= j && r <= j + k - 1) || (g >= j && r <= g + k - 1) || (b >= j && b <= j + k - 1))
+			sym = a[i];
+		j += k;
+	}
+
+	return sym;
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "rus");
+
+	string file_d;
+	string img_d;
+	int kw = 2;
+	int kh = 6;
+
+	cout << "Имя и директория для фото: " << endl;
+	cin >> img_d;
+
+	cout << "Имя и директория выходного файла: " << endl;
+	cin >> file_d;
+
+	sf::Image image;
+	image.loadFromFile(img_d);
+
+	ofstream fout(file_d);
+
+	const int h = image.getSize().y;
+	const int w = image.getSize().x;
+	char** img = new char* [w];
+
+	for (int i = 0; i < h; i += kh)
+		img[i] = new char[h];
+
+	if (w < 400)
+		kh = kw = 1;
+
+	for (int i = 0; i < h; i += kh) {
+		for (int j = 0; j < w; j += kw) {
+			sf::Color color = image.getPixel(j, i);
+
+			int r = color.r;
+			int g = color.g;
+			int b = color.b;
+
+			**img = symbol(r, g, b);
+
+			if (w < 400)
+				fout << **img << **img;
+			else
+				fout << **img;
+		}
+		fout << '\n';
+	}
+
+	fout.close();
+	system("pause");
+	cin.get();
+
+	return 0;
+}
